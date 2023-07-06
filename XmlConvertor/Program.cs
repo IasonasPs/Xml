29620325e543c94e72c44b0d10dae08aa646b6ee
Iasonas <iasonas.ps@gmail.com>
2023-07-06 11:17:10 +0300

    Final

---
+++ XmlConvertor/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using XmlConvertor.services;
+using XmlReader;
+
+
+namespace XmlConvertor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            string filename = "xml.txt";
+            string  path = @"..\..\..\..\XmlReader\XmlFiles";
+            var fullPath = Path.GetFullPath(path);
+
+
+            string xml = Find.FindFile(path, filename);
+            string result = Convertor.XmlToJson(xml);
+            Save.SaveToFile(result, "jsonfromXml.txt", true);
+
+
+            string json = Find.FindFile(desktop, "jsonfromXml.txt");
+            result = Convertor.JsonToXml(json);
+            Save.SaveToFile(result, "XmlFromJson.txt", true);
+
+            Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - ");
+            Console.WriteLine("All files saved successfully!!");
+            Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - ");
+
+        }
+    }
+}
---
+++ XmlConvertor/services/Convertor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+
+
+namespace XmlConvertor.services
+{
+    public class Convertor
+    {
+        public static string XmlToJson(string xml)
+        {
+            XmlDocument doc = new XmlDocument();
+
+            doc.LoadXml(xml);
+
+            string json = JsonConvert.SerializeObject(doc);
+            return json;
+        }
+        private static readonly XDeclaration _defaultDeclaration = new("1.0", null, null);
+        public static string JsonToXml(string text)
+        {
+            //Console.WriteLine("----------------------------------------------------------------");
+            var doc = JsonConvert.DeserializeXNode(text)!;
+            var declaration = doc.Declaration ?? _defaultDeclaration;
+
+            string xml = $"{declaration}{Environment.NewLine}{doc}";
+
+            return xml;
+        }
+    }
+}
